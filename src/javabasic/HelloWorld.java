6c9ae8bcbffccff598446a66972e4b5404fc886f
Kazi Md. Rakibul Hassan <kazimdrakibulhasan@gmail.com>
2024-05-23 10:30:41 +0600

    helloworld

---
+++ src/javabasic/HelloWorld.java
@@ -0,0 +1,13 @@
+package javabasic;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //display my name
+        System.out.println("Kazi Md. Rakibul Hassan");
+        // display my phone number
+        System.out.println("01580351798");
+        System.out.println("hello \neveryone \nBSc in CS");
+        // display how to give 4 spaces in a sentence
+        System.out.println("1 \t2");
+    }
+}
