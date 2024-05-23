87fc8d0bf4ab0b83a58198032d7e3473a22a78fa
Kazi Md. Rakibul Hassan <kazimdrakibulhasan@gmail.com>
2024-05-23 11:37:22 +0600

    datatypes

---
+++ src/javabasic/DataType.java
@@ -0,0 +1,19 @@
+package javabasic;
+
+public class DataType {
+    public static void main(String[] arg){
+        int num = 10; // dynamic initialization
+        byte num1 = 100;
+        boolean b;
+        char c;
+        short s = 1250;
+        float f = 10.55f;
+        double d = 23.4765765;
+        int i = 78;
+
+        b = true;
+        System.out.println("b = "+b);
+        c = 'a';
+        System.out.println("c = "+c);
+    }
+}
